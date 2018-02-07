8ade78771ebf648208b3d32c67ac3b71b2a8be15
pmtavares <pmauriciotavares@yahoo.com.br>
2018-02-07 21:40:51 +0000

    Added Bootstrap plus events handling with classes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,18 +8,42 @@ import { CoursesService } from './courses.service';
       <ul>
         <li *ngFor="let course of courses">{{ course }}</li>
       </ul>
-
+      <img src="{{ imageUrl}}"/>
+      <table>
+        <tr>
+          <td [attr.colspan]="25">table1</td>
+        </tr>
+      </table>
+      <button class="btn btn-primary" (click)="onSave($event)">Save</button>
+    <div (click)="divClick()">
+      <button class="btn" [class.active]="isActive" (click)="onButtonClick($event)">Active</button>
+    </div>
+    <button [style.backgroundColor]="isActive ? 'blue': 'white'">Active</button>
     `
   
 })
 export class CoursesComponent {
   title = "List of courses";
   courses;
-
+  imageUrl = "https://loremflickr.com/200/230";
+  isActive = true;
   constructor(service: CoursesService) { //in order for the dependency injection to work, we need to declare in the app module, providers array
     
     this.courses = service.getCourses();
 
+  }
+  onButtonClick($event) {
+    $event.stopPropagation();
+    console.log("Active was clicked");
+  }
+
+  divClick() {
+    
+    console.log("Div click");
+  }
+  onSave($event) {
+    alert("saved!");
+    console.log($event);
   }
 
   getTitle() {
