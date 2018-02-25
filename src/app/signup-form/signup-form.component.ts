1830b95bea720b050be6dd6d7d70422e288459d8
pmtavares <pmauriciotavares@yahoo.com.br>
2018-02-25 17:55:28 +0000

    Form Validation (ngSubmit)

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsernameValidators } from './username.validators';
+
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
+
 export class SignupFormComponent {
   myForm = new FormGroup({
     'username': new FormControl('', [Validators.required, Validators.minLength(3),
-      UsernameValidators.cannotContainsSpace], UsernameValidators.shouldBeUnique),
+    UsernameValidators.cannotContainsSpace], UsernameValidators.shouldBeUnique),
     'password': new FormControl('', Validators.required)
   });
+
+  login() {
+    this.myForm.setErrors({
+      invalidLogin: true
+    });
+  }
 
   get username() {
     return this.myForm.get('username');
   }
-
 }
