67754dd9d3e7a2626d74af549d51d6160d2503eb
pmtavares <pmauriciotavares@yahoo.com.br>
2018-02-25 18:37:02 +0000

    FormArray click and delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PanelComponent } from './panel/panel.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
     PanelComponent,
     InputFormatDirective,
     ContactFormComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent {
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
+
+  removeTopic(topic: FormControl) {
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+}
