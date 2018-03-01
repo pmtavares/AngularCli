7d21757356229c3b5d21e42c5bfb076e0ebef639
pmtavares <pmauriciotavares@yahoo.com.br>
2018-03-01 19:38:46 +0000

    Added Get and Post HTTP requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { InputFormatDirective } from './input-format.directive';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
@@ -30,12 +32,14 @@ import { NewCourseFormComponent } from './new-course-form/new-course-form.compon
     InputFormatDirective,
     ContactFormComponent,
     SignupFormComponent,
-    NewCourseFormComponent
+    NewCourseFormComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent  {
+  posts: any[];
+  private url = "http://jsonplaceholder.typicode.com/posts";
+
+  constructor(private http: Http) {
+    http.get(this.url).subscribe(response => {
+      this.posts = response.json();
+    });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+
+    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
+      console.log(response.json());
+      post['id'] = response.json().id;
+      this.posts.splice(0, 0, post);
+    });
+  }
+
+
+}
