2f827968a4130f5dfd37662895eabdd7ea175f9c
pmtavares <pmauriciotavares@yahoo.com.br>
2018-03-15 22:01:26 +0000

    Added Error handling on the components

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -16,36 +16,72 @@ export class PostComponent implements OnInit{
   }
 
   ngOnInit() {
-    this.service.getPosts().subscribe(response => {
+    this.service.getPosts().subscribe(
+      response => {
       this.posts = response.json();
-    });
+    },
+      error => {
+        alert("An unexpected error ocurred");
+        console.log(error);
+      });
   }
 
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
 
-    this.service.createPost(post).subscribe(response => {
+    this.service.createPost(post).subscribe(
+      response => {
       console.log(response.json());
       post['id'] = response.json().id;
       this.posts.splice(0, 0, post);
-    });
+    },
+      (error: Response) => {
+        if (error.status === 404) {
+          //this.form.setErrors(error.json()); COMMENTED BECAUSE WE DONT HAVE A FORM
+        }
+        else {
+          alert("An unexpected error ocurred for delete post");
+          console.log(error);
+        }
+       
+
+      });
   }
 
   updatePost(post) {
     //this.http.put(this.url, JSON.stringify(post));
     
-      this.service.updatePost(post).subscribe(response => {
+    this.service.updatePost(post).subscribe(
+      response => {
         console.log(response.json());
+      },
+      error => {
+        alert("An unexpected error ocurred for update post");
+        console.log(error);
+
       });
    
 
   }
   deletePost(post) {
-     this.service.deletePost(post.id).subscribe(response => {
+    this.service.deletePost(post.id).subscribe(
+      response => {
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
-      });
+      },
+      (error: Response) => {
+        if (error.status === 404) {
+          alert("This item has already been deleted");
+        }
+        else {
+          alert("An unexpected error ocurred for delete post");
+          console.log(error);
+        }
+
+      
+
+    });
   }
 
 }
